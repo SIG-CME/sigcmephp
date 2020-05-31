4d6dbfc68467920cb6d6508ea35d90da8b3ef149
Eliezer Zarpelao <zarp_ezarpelao@orizon.com.br>
2020-05-31 13:36:43 -0300

    Texto Sobre

--- views/site/about.php
+++ views/site/about.php
@@ -4,33 +4,33 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'Sobre';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-    A informática, no mundo contemporâneo, vem se concretizando como uma das principais ferramentas de
-trabalho em todas as áreas de conhecimento, dentre elas a de saúde. Dentro do ambiente intra-hospitalar
-diversos serviços de apoio contribuem para a execução de sua atividade finalística, dentre estes, o Centro
-de Material e Esterilização (CME). O CME é uma unidade funcional de apoio técnico destinado ao
-processamento de produtos para saúde. É considerada uma área crítica, que desempenha um conjunto de
-ações relacionadas à recepção, limpeza, secagem, avaliação da integridade e da funcionalidade, preparo,
-desinfecção ou esterilização, armazenamento e distribuição de artigos para as unidades consumidoras.
-Todas estas atividades são muito complexas, portanto os fluxos e processos do CME devem ser
-monitorizados. Na lógica de reorganizar as atividades, simplificando e padronizando os processos, a
-informatização aliada A ́s ferramentas do lean healthcare promove a dinamização dos fluxos, maior
-controle das etapas e favorece o gerenciamento da informação. A informatização pode trazer inúmeras
-vantagens para as organizações de assistência à saúde, melhorando as informações coletadas, ajudando a
-definir a real demanda e a construir modelos funcionais e seguros. Diante da carência de um modelo
-aplicado à gestão dos registros dos fluxos e processos em CME, a proposta deste sistema informatizado é
-o desenvolvimento de uma tecnologia aplicada a partir da implementação dos conceitos <i>lean healthcare</i>
-em CME aliada à metodologia <i>scrum</i> para desenvolvimento de softwares. Deste modo, a informatização
-dos dados gerados permite que as informações relacionadas a cada etapa do processo sejam
-disponibilizadas em tempo real, comportando-se como um instrumento gerencial organizado das
-informações e dos recursos envolvidos nos processos de trabalho do CME, permitindo análises com vistas
-à melhoria contínua e aprimoramento.
+        A informática, no mundo contemporâneo, vem se concretizando como uma das principais ferramentas de
+        trabalho em todas as áreas de conhecimento, dentre elas a de saúde. Dentro do ambiente intra-hospitalar
+        diversos serviços de apoio contribuem para a execução de sua atividade finalística, dentre estes, o Centro
+        de Material e Esterilização (CME). O CME é uma unidade funcional de apoio técnico destinado ao
+        processamento de produtos para saúde. É considerada uma área crítica, que desempenha um conjunto de
+        ações relacionadas à recepção, limpeza, secagem, avaliação da integridade e da funcionalidade, preparo,
+        desinfecção ou esterilização, armazenamento e distribuição de artigos para as unidades consumidoras.
+        Todas estas atividades são muito complexas, portanto os fluxos e processos do CME devem ser
+        monitorizados.</p>
+    <p> Na lógica de reorganizar as atividades, simplificando e padronizando os processos, a
+        informatização aliada às ferramentas do <i>lean healthcare</i> promovem a dinamização dos fluxos, maior
+        controle das etapas e favorece o gerenciamento da informação. A informatização pode trazer inúmeras
+        vantagens para as organizações de assistência à saúde, melhorando as informações coletadas, ajudando a
+        definir a real demanda e a construir modelos funcionais e seguros.</p>
+    <p> Diante da carência de um modelo aplicado à gestão dos registros dos fluxos e processos em CME, a proposta deste sistema informatizado é
+        o desenvolvimento de uma tecnologia aplicada a partir da implementação dos conceitos <i>lean healthcare</i>
+        em CME aliada à metodologia <i>scrum</i> para desenvolvimento de softwares. Deste modo, a informatização
+        dos dados gerados permite que as informações relacionadas a cada etapa do processo sejam
+        disponibilizadas em tempo real, comportando-se como um instrumento gerencial organizado das
+        informações e dos recursos envolvidos nos processos de trabalho do CME, permitindo análises com vistas
+        à melhoria contínua e aprimoramento.
     </p>
-
 </div>
