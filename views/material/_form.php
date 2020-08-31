f71b2a2a68185ed254f282a23e14f8a3e7088ff7
Eliezer Zarpelao <zarp_ezarpelao@orizon.com.br>
2020-08-30 22:49:16 -0300

    Correcao Save Material

--- views/material/_form.php
+++ views/material/_form.php
@@ -22,7 +22,7 @@ $listCategoria=ArrayHelper::map($categorias,'id','descricao');
     <?= $form->field($model, 'categoriaid')->dropDownList($listCategoria) ?>
       
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
